b7838c7f600f118e9bda11dd198591efc1d5046c
Jack5241027 <jack5241027@gmail.com>
2019-03-28 00:00:47 +0800

    Update config.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 module.exports = {
   parser: "babel-eslint",
+  extends: [
+    "./rules/best-practices",
+    "./rules/errors",
+    "./rules/es6",
+    "./rules/import",
+    "./rules/react",
+    "./rules/style",
+    "./rules/variables"
+  ].map(require.resolve),
   env: {
     browser: true,
     jest: true
